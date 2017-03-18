681bfd7aeb7ed87806707adb05813f5533eb1e16
jtang10 <tangjingning@gmail.com>
2017-03-18 15:18:01 -0500

    add several locations

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
+var locations = [
+    {
+        title: 'Yolk - Streeterville',
+        lat: 41.8910135,
+        lng: -87.6165893,
+        id: 'ChIJW7gEhFUrDogRmZrarW7wmE8'
+    },
+    {
+        title: 'Cite',
+        lat: 41.8910135,
+        lng: -87.6165893,
+        id: 'ChIJLzy5vlMrDogR1EZU12Hh8qk'
+    },
+    {
+        title: 'Tanta Chicago',
+        lat: 41.8920694,
+        lng:-87.6206376,
+        id: 'ChIJdeoev7MsDogRZOJFBNqupKQ'
+    },
+    {
+        title: 'Purple Pig',
+        lat: 41.8920694,
+        lng: -87.6206376,
+        id: 'ChIJl8NTEawsDogRwXH-IVDyH2A'
+    }
+    {
+        title: 'Capital Grille',
+        lat: 41.8942018,
+        lng: -87.6193394,
+        id: 'ChIJo9QCaqssDogRmmJ0MAf-aeE'
+    },
+    {
+        title: 'RPM Italian',
+        lat: 41.8902564,
+        lng: -87.6271285,
+        id: 'ChIJgfrOPLIsDogRvM0_GwLBpyI'
+    }
+]
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
